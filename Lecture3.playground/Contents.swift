e404d27e7cd97bd9e83ef7e4950bbf135b476560
michaela10c <michaela10c@qq.com>
2016-06-27 17:55:07 -0400

    lecture with swift!

--- Lecture3.playground/Contents.swift
+++ Lecture3.playground/Contents.swift
@@ -27,12 +27,12 @@ appleSummary = "I have \(oranges) oranges." // \ - parse to string
 
 var aFloat : Float
 
-var shoppingList: Array<Any> = ["catfish", "water", "tulips", "blue paint"]
-shoppingList.append("toothpaste")
-shoppingList.append("James M. Howard")
-shoppingList.append(99)
-shoppingList.last
+var shoppingList: NSMutableArray = ["catfish", "water", "tulips", "blue paint"] //Next Step Mutable in CLASS
+var copyList = shoppingList
 
+shoppingList.addObject(47)
+
+copyList
 var occupation: Dictionary<String,String> = [
     "malcom": "captain",
     "kalyee": "mechanic"
@@ -40,4 +40,63 @@ var occupation: Dictionary<String,String> = [
 
 occupation["Jayne"] = "Public Relations"
 
-occupation
+/////
+var occupationNames = occupation.map {$0.0}.map {$0; "blah"} //Coolest way!!!
+occupationNames
+
+
+var r = 50...100
+var g = r.generate()
+g.next()
+g.next()
+
+var g1 = [11,21,31,41,51].generate()
+
+var tuple1 = (1,2)
+tuple1.0
+tuple1.1
+
+
+var tuple2 = (first:"van", last: "Simmons")
+tuple2.first
+tuple2.last
+
+for(l,v) in occupation {
+    print("\(l),\(v)")
+}
+
+func doubler(x:Int) -> Int{
+    return x*2
+}
+
+func doubleDoubler(x:Double) -> Double {
+    return x*2
+}
+
+doubler(4)
+doubleDoubler(3)
+
+func progression(v:Int,f: Int->Int) -> Int{
+    return f(v)
+}
+
+progression(4,f:doubler)
+
+
+//!!!!!!
+var tf = false
+tf = true
+
+var arrArr:Array<Dictionary<Int,Bool>> = [[1:true]]
+
+var closure = { (x:Int) -> Int in
+    return x*2
+}
+
+closure(6)
+
+progression(6,f:closure)
+
+progression(6, f: {(x:Int) -> Int in
+    return x*2
+})
