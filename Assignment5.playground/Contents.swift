7374947cf571611bd5b38e3e1fc419fc6f3e8adb
michaela10c <michaela10c@qq.com>
2016-07-24 20:41:20 -0400

    Assignment5 Complete!!!!!!

--- Assignment5.playground/Contents.swift
+++ Assignment5.playground/Contents.swift
@@ -11,10 +11,9 @@ func isLeap(year: Int) -> Bool{
 
 func julianDate(year: Int, month: Int, day: Int) -> Int{//get leap year condition
     let yearDays = (1900..<year).map({(number:Int)->Int in return isLeap(number) ? 366 : 365}).reduce(0, combine: +)
-    let monthDays = monthDaysInYear[0..<month].reduce(0, combine: +)
+    let monthDays = monthDaysInYear[0..<month].map({(number:Int)->Int in return isLeap(year)&&number==28 ? 29 : number}).reduce(0, combine: +)
     return yearDays + monthDays + day
 }
-
 //Inspired by Nathan Guerin's section
 
 isLeap(1960)
@@ -24,5 +23,7 @@ isLeap(2000)
 
 julianDate(1960, month: 9, day: 28)
 julianDate(1900, month: 1, day: 1)
-julianDate(2000, month: 12, day: 31)
+julianDate(1900, month: 12, day: 31)
+julianDate(1901, month: 1, day: 1)
+julianDate(1901, month: 1, day: 1) - julianDate(1900, month: 1, day: 1)
 julianDate(2001, month: 1, day: 1) - julianDate(2000, month: 1, day: 1)
